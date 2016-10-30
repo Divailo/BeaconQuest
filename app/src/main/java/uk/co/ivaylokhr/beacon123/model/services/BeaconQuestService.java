6b84cc4b52a8158737fc020193a91d96e3ed0773
Ivo <ivaylokhr@gmail.com>
2016-10-30 09:14:27 +0000

    Service stuff deployed

--- app/src/main/java/uk/co/ivaylokhr/beacon123/model/services/BeaconQuestService.java
+++ app/src/main/java/uk/co/ivaylokhr/beacon123/model/services/BeaconQuestService.java
@@ -1,11 +1,46 @@
 package uk.co.ivaylokhr.beacon123.model.services;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 /**
  * Created by Ivaylo on 30/10/2016.
  */
 
 public interface BeaconQuestService {
 
-//    @GET("users/{user}/repos")
-//    Call<List<Repo>> listRepos(@Path("user") String user);
+    @FormUrlEncoded
+    @POST("/challengeNew")
+    Call<ResponseBody> registerBeacon(@Field("accountId") String userid,
+                                      @Field("beaconId") String beaconid
+    );
+
+    @FormUrlEncoded
+    @POST("/challengeAnswer")
+    Call<ResponseBody> challengeAnswer(@Field("accountId") String userid,
+                                      @Field("beaconId") String beaconid,
+                                       @Field("answer") String answer
+    );
+
+    @FormUrlEncoded
+    @POST("/challengeCancel")
+    Call<ResponseBody> challengeAnswer(@Field("accountId") String userid,
+                                       @Field("beaconId") String beaconid
+    );
+
+    @FormUrlEncoded
+    @POST("/registerAccount")
+    Call<ResponseBody> registerAccount(@Field("username") String username,
+                                       @Field("email") String mail,
+                                       @Field("password") String password
+    );
+
+    @FormUrlEncoded
+    @POST("/login")
+    Call<ResponseBody> login(@Field("username") String username,
+                             @Field("password") String password
+    );
 }
